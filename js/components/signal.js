c5747cb4b0d8f93a27bc5b5c513e2f6fcda7a8a0
Acks <AcksBerg@github.com>
2025-01-24 12:41:53 +0100

    TODO für die Signalverarbeitung hinzugefügt um die loop protection in der Zukunft zu verbessern.

--- js/components/signal.js
+++ js/components/signal.js
@@ -13,6 +13,7 @@ class Signal{
         // Die Logik wenn ein Signal ein objekt "besucht".
         // Returned es false wurde das objekt schon vorher von dem Element besucht und es muss ein Fehler ausgegeben werden
         // Returned es true wurde das Signal erfolgreich verarbeitet
+        // TODO Möglichkeit hinzufügen um Signale zu kombinieren damit diese nach einem Gater noch immer eine Loop protection haben
         if(this.visited.includes(obj)){
             return false;
         }else{
