4c5a48d7bcf252a74661cd5d1be4d08f52696b77
AcksBerg <5879689+AcksBerg@users.noreply.github.com>
2025-02-22 11:12:14 +0100

    Fix: Man kann nicht mehr durch die HTML-Elemente hindurch ICs verschieben.

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,12 @@
 // Die Aktionen der Menüleiste
 // Namensconverntion btn_reiter_aktion
 // Click auf Navbar unterbinden damit man nicht hinter das HTML Element Clicken kann
-document.querySelector("nav").addEventListener("click", (e) => {
-  console.log("Navbar click");
+const nav = document.querySelector("nav");
+nav.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
+
+nav.addEventListener("mousedown", (e) => {
   e.stopPropagation();
 });
 
@@ -20,16 +24,27 @@ document
   });
 
 // Hilfe
+const hilfe_popup = document.querySelector("#hilfe_popup");
+const hilfe_popup_btn = document.querySelector("#hilfe_popup button");
+
 document.querySelector("#btn_hilfe").addEventListener("click", (e) => {
-  document.querySelector("#help").style.display = "block";
+  hilfe_popup.style.display = "block";
   e.stopPropagation();
 });
 
-document.querySelector("#help").addEventListener("click", (e) => {
+hilfe_popup.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-document.querySelector("#help button").addEventListener("click", (e) => {
-  document.querySelector("#help").style.display = "none";
+hilfe_popup.addEventListener("mousedown", (e) => {
+  e.stopPropagation();
+});
+
+hilfe_popup_btn.addEventListener("click", (e) => {
+  hilfe_popup.style.display = "none";
+  e.stopPropagation();
+});
+
+hilfe_popup_btn.addEventListener("mousedown", (e) => {
   e.stopPropagation();
 });
