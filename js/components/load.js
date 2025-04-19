f45b8bf979f09f31eb39b0ec6088fbaae30ceeb9
AcksBerg <AcksBerg@users.noreply.github.com>
2025-04-19 12:50:55 +0200

    fix: Load funktion kann jetzt auch ICs erstellen die vorher nicht in der Sketch waren.

--- js/components/load.js
+++ js/components/load.js
@@ -1,10 +1,8 @@
-
 class Load {
   /**
    * Klasse zum Laden der Kabel und ICs
    */
   constructor() {
-
     this.fileInput = createFileInput(this.handleFile.bind(this));
     this.fileInput.hide(); // damit kein element auf website angezeigt wird
     this.fileInput.elt.addEventListener("click", function (e) {
@@ -48,9 +46,10 @@ class Load {
       });
       console.log(this.start_pin, this.links, this.end_pin, this.color);
       cables.push(
-        new Cable(this.start_pin, this.links, this.end_pin, this.color));
-        this.start_pin.connect(this.end_pin);
-        this.end_pin.connect(this.start_pin);
+        new Cable(this.start_pin, this.links, this.end_pin, this.color)
+      );
+      this.start_pin.connect(this.end_pin);
+      this.end_pin.connect(this.start_pin);
     });
   }
   create_ic() {
@@ -60,25 +59,50 @@ class Load {
      * schleife über alle sockel wenn ic name im load gefunden
      * wenn sockel id passt, dann wird der ic in den sockel gesetzt
      */
-    this.icdata.forEach(element => {
-      this.socket_id_load = element[0]
-      this.socket_at_load = element[1]
-      this.ic_name_load = element[2]
-      ics.forEach(ic =>
-        {
-          if (ic.name === this.ic_name_load)
-    
-            {
-              platinElementsSocket.forEach(socket => 
-                {
-                if(socket.id === this.socket_id_load)
-                  {
-                    console.log(ic,socket)
-                    console.log("gefunden")
-                    ic.connectWithSocketAtPin(socket,this.socket_at_load)
-                  }
-                })
-            }        
+    this.icdata.forEach((element) => {
+      this.socket_id_load = element[0];
+      this.socket_at_load = element[1];
+      this.ic_name_load = element[2];
+      // ic daten aus icInfos laden.
+      const info = icInfos[parseInt(this.ic_name_load.split(" ")[1])]
+      // pins / 2 da ics rows wollen und keine pin anzahl
+      const neuerIc = new Ic(getScreenCenter(), info.pins / 2, info.name);
+      ics.push(neuerIc);
+      info.gates?.forEach((gate) => {
+        const gateObj =
+          gate.type === "AND"
+            ? new And()
+            : gate.type === "XOR"
+            ? new Xor()
+            : gate.type === "OR"
+            ? new Or()
+            : gate.type === "NOT"
+            ? new Not()
+            : null;
+        if (!gateObj) return;
+
+        // Mappe Pin-Referenzen zu den richtigen Connectors
+        const connectorObjs = gate.connectors.map((idx) => {
+          if (typeof idx === "string" && idx.startsWith("i")) {
+            const i = parseInt(idx.slice(1));
+            return neuerIc.connectorsPlugInvisible[i];
+          } else {
+            return neuerIc.connectorsPlug[idx];
+          }
+        });
+
+        neuerIc.addGate(gateObj, connectorObjs);
+      });
+      ics.forEach((ic) => {
+        if (ic.name === this.ic_name_load) {
+          platinElementsSocket.forEach((socket) => {
+            if (socket.id === this.socket_id_load) {
+              console.log(ic, socket);
+              console.log("gefunden");
+              ic.connectWithSocketAtPin(socket, this.socket_at_load);
+            }
+          });
+        }
       });
     });
   }
